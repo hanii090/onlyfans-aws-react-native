cdbdc36e186ad0f33ce9e9c8609b365f043dca05
hanii090 <24.hours.hanii@gmail.com>
2023-07-28 11:17:47 +0500

    first commit

---
+++ assets/data/posts.js
@@ -0,0 +1,58 @@
+export default [
+  {
+    id: '1',
+    User: {
+      id: '8',
+      name: 'nothanii',
+      handle: 'nothanii2',
+      avatar:
+        'https://nothanii.s3.amazonaws.com/Mine/131614433.jpeg',
+    },
+    created_at: '2022-03-29T18:30:00Z',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dictum nulla vel metus ullamcorper, id lobortis risus luctus.',
+    image: 'https://nothanii.s3.us-east-2.amazonaws.com/images/1.jpg',
+    likes: 10,
+  },
+  {
+    id: '2',
+    User: {
+      id: '8',
+      name: 'nothanii',
+      handle: 'nothanii2',
+      avatar:
+        'https://nothanii.s3.amazonaws.com/Mine/131614433.jpeg',
+    },
+    created_at: '2022-03-30T12:00:00Z',
+    text: 'Pellentesque et purus nec magna fringilla feugiat. Fusce malesuada faucibus urna, eu ultrices arcu rhoncus at.',
+    image: 'https://nothanii.s3.us-east-2.amazonaws.com/images/2.jpg',
+    likes: 5,
+  },
+  {
+    id: '3',
+    User: {
+      id: '8',
+      name: 'nothanii',
+      handle: 'nothanii2',
+      avatar:
+        'https://nothanii.s3.amazonaws.com/Mine/131614433.jpeg',
+    },
+    created_at: '2022-03-31T08:15:00Z',
+    text: 'In non augue finibus, sagittis dolor eget, tristique ipsum. Curabitur ut enim nec nulla sodales consectetur.',
+    image: 'https://nothanii.s3.us-east-2.amazonaws.com/images/3.jpg',
+    likes: 20,
+  },
+  {
+    id: '4',
+    User: {
+      id: '8',
+      name: 'nothanii',
+      handle: 'nothanii2',
+      avatar:
+        'https://nothanii.s3.amazonaws.com/Mine/131614433.jpeg',
+    },
+    created_at: '2022-03-31T10:45:00Z',
+    text: 'Suspendisse vitae vehicula nisi. Nam dignissim, est sed ultrices commodo, magna quam euismod nibh, vitae commodo ante elit nec leo.',
+    image: 'https://nothanii.s3.us-east-2.amazonaws.com/images/4.jpg',
+    likes: 15,
+  },
+];
